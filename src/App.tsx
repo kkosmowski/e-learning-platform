5cbdd8a43a5568e9ebf464182d276ebc071445c2
Krzysztof Kosmowski <63412876+kkosmowski@users.noreply.github.com>
2022-06-16 19:02:14 +0200

    chore: initial configuration (#3)
    * chore: install and config eslint
    
    * chore: install and config prettier and husky
    
    format existing files
    
    * chore: improve typescript configuration
    
    * chore: create basic mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Box, Typography } from '@mui/material';
-import { Rocket } from '@mui/icons-material';
+import { Box, Button } from '@mui/material';
+import Dashboard from 'components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Box sx={ { display: 'flex', justifyContent: 'center', alignItems: 'center', height: 64 } }>
-        <Typography>Hello there</Typography>
-        <Rocket />
+      <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+        <Button color="primary">primary</Button>
+        <Button color="secondary">secondary</Button>
+        <Button color="success">success</Button>
+        <Button color="info">info</Button>
+        <Button color="warning">warning</Button>
+        <Button color="error">error</Button>
+      </Box>
+      <span>Regular text here</span>
+      <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+        <Button variant="contained" color="primary">
+          primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="warning">
+          warning
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+      </Box>
+      <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
+        <Button variant="outlined" color="primary">
+          primary
+        </Button>
+        <Button variant="outlined" color="secondary">
+          secondary
+        </Button>
+        <Button variant="outlined" color="success">
+          success
+        </Button>
+        <Button variant="outlined" color="info">
+          info
+        </Button>
+        <Button variant="outlined" color="warning">
+          warning
+        </Button>
+        <Button variant="outlined" color="error">
+          error
+        </Button>
       </Box>
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Editt <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -25,6 +69,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Dashboard />
     </div>
   );
 }
---
+++ src/colors.ts
@@ -0,0 +1,142 @@
+export const primary = {
+  50: '#fefeff',
+  100: '#c6cbeb',
+  200: '#9ca6dd',
+  300: '#6776ca',
+  400: '#5062c2',
+  500: '#3f51b5',
+  600: '#37479e',
+  700: '#2f3d88',
+  800: '#273371',
+  900: '#1f285a',
+};
+
+export const secondary = {
+  50: '#ffffff',
+  100: '#f1f3f4',
+  200: '#d2d7dc',
+  300: '#aab3bc',
+  400: '#99a4ae',
+  500: '#8895a1',
+  600: '#778693',
+  700: '#687684',
+  800: '#5b6773',
+  900: '#4d5862',
+};
+
+export const success = {
+  50: '#eaf5ea',
+  100: '#c9e7cb',
+  200: '#a6d7a8',
+  300: '#82c785',
+  400: '#67bb6a',
+  500: '#4caf50',
+  600: '#45a849',
+  700: '#3c9f40',
+  800: '#339637',
+  900: '#248627',
+};
+
+export const info = {
+  50: '#e1f5fe',
+  100: '#b3e5fc',
+  200: '#81d4fa',
+  300: '#4fc3f7',
+  400: '#29b6f6',
+  500: '#03a9f4',
+  600: '#039be5',
+  700: '#0288d1',
+  800: '#0277bd',
+  900: '#01579b',
+};
+
+export const warning = {
+  50: '#ffffff',
+  100: '#ffe4bd',
+  200: '#ffce85',
+  300: '#ffb13d',
+  400: '#ffa41f',
+  500: '#ff9800',
+  600: '#e08600',
+  700: '#c27400',
+  800: '#a36100',
+  900: '#854f00',
+};
+
+export const error = {
+  50: '#f9e9e8',
+  100: '#efc7c5',
+  200: '#e5a29e',
+  300: '#da7d77',
+  400: '#d26159',
+  500: '#ca453c',
+  600: '#c53e36',
+  700: '#bd362e',
+  800: '#b72e27',
+  900: '#ab1f1a',
+};
+
+export const background = {
+  50: '#fff',
+  100: '#fafafa',
+  200: '#f4f4f4',
+  300: '#ececec',
+  400: '#e4e4e4',
+  500: '#dfdfdf',
+  600: '#d9d9d9',
+  700: '#d5d5d5',
+  800: '#cfcfcf',
+  900: '#cacaca',
+};
+
+export const text = {
+  50: '#000',
+  100: '#0f0f0f',
+  200: '#161616',
+  300: '#1f1f1f',
+  400: '#242424',
+  500: '#2b2b2b',
+  600: '#323232',
+  700: '#393939',
+  800: '#484848',
+  900: '#5a5a5a',
+  1000: '#fff',
+};
+
+const colors = {
+  background: {
+    default: background[50],
+  },
+  error: {
+    main: error[500],
+    contrastText: text[1000],
+  },
+  info: {
+    main: info[500],
+    contrastText: text[1000],
+  },
+  mode: 'light',
+  primary: {
+    main: primary[500],
+    contrastText: text[1000],
+  },
+  secondary: {
+    main: secondary[500],
+    contrastText: text[1000],
+  },
+  success: {
+    main: success[500],
+    contrastText: text[1000],
+  },
+  text: {
+    primary: primary[500],
+    secondary: secondary[500],
+    disabled: text[900],
+  },
+  warning: {
+    main: warning[500],
+    contrastText: text[1000],
+  },
+};
+
+export default colors;
