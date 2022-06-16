d4ec1dc509b9b6ad5a2456d45910e63198f1689f
kkosmowski <krzysztof@kosmowski.ct8.pl>
2022-06-16 19:00:25 +0200

    chore: create basic mui theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   root
 );
---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+import { createMuiTheme, ThemeOptions } from '@mui/material';
+import colors from './colors';
+
+const baseThemeOptions: ThemeOptions = {
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 0,
+          paddingTop: 4,
+          paddingBottom: 4,
+        },
+        contained: {
+          boxShadow: 'none',
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: 'Lato, sans-serif',
+  },
+  palette: {
+    background: colors.background,
+    error: colors.error,
+    info: colors.info,
+    mode: 'light',
+    primary: colors.primary,
+    secondary: colors.secondary,
+    success: colors.success,
+    text: colors.text,
+    warning: colors.warning,
+  },
+};
+
+const theme = createMuiTheme(baseThemeOptions);
+
+export default theme;
