556ce6227b78195ad1e453475e95b379eba75636
kkosmowski <krzysztof@kosmowski.ct8.pl>
2022-06-16 13:10:34 +0200

    chore: install fundamental dependencies
    avoid yarn.lock in the repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Box, Typography } from '@mui/material';
+import { Rocket } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
+      <Box sx={ { display: 'flex', justifyContent: 'center', alignItems: 'center', height: 64 } }>
+        <Typography>Hello there</Typography>
+        <Rocket />
+      </Box>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
