d7e84143893ef980a4ad21fe95a5e906c84656e1
kkosmowski <krzysztof@kosmowski.ct8.pl>
2022-11-13 17:42:27 +0100

    refactor: improve task time left

--- src/shared/utils/date.utils.ts
+++ src/shared/utils/date.utils.ts
@@ -1,30 +1,17 @@
 import { TFunction } from 'i18next';
-import {
-  differenceInMinutes,
-  differenceInSeconds,
-  minutesToHours,
-} from 'date-fns';
+import { differenceInMinutes } from 'date-fns';
 
 import { HOURS_IN_A_DAY, MINUTE, MINUTES_IN_AN_HOUR } from 'shared/consts/date';
 
 export const timeLeft = (t: TFunction, deadline: Date): [string, number] => {
-  const diffInSeconds = differenceInSeconds(deadline, new Date());
   const diffInMinutes = differenceInMinutes(deadline, new Date());
-  let timeLeftString;
-
-  if (diffInSeconds <= 0) {
-    timeLeftString = t('pastDeadline');
-  } else if (diffInMinutes > MINUTES_IN_AN_HOUR * HOURS_IN_A_DAY) {
-    timeLeftString = `${Math.ceil(
-      minutesToHours(diffInMinutes) / HOURS_IN_A_DAY
-    )} ${t('daysLeft')}`;
-  } else if (diffInMinutes > MINUTES_IN_AN_HOUR) {
-    timeLeftString = `${Math.ceil(minutesToHours(diffInMinutes))} ${t(
-      'hoursLeft'
-    )}`;
-  } else {
-    timeLeftString = `${diffInMinutes} ${t('minutesLeft')}`;
-  }
+  const timeLeftString =
+    diffInMinutes < 0
+      ? t('pastDeadline')
+      : getReadableTimeDifference(deadline, new Date(), t, {
+          shouldRound: true,
+          short: true,
+        });
 
   return [timeLeftString, diffInMinutes];
 };
@@ -41,13 +28,24 @@ export const isDateAfterAnother = (date: Date, anotherDate: Date): boolean =>
 export const getReadableTimeDifference = (
   date1: Date,
   date2: Date,
-  t: TFunction
+  t: TFunction,
+  options?: { shouldRound?: boolean; short?: boolean }
 ) => {
-  return getReadableDuration((date1.getTime() - date2.getTime()) / MINUTE, t);
+  return getReadableDuration(
+    (date1.getTime() - date2.getTime()) / MINUTE,
+    t,
+    options
+  );
 };
 
-export const getReadableDuration = (_duration: number, t: TFunction) => {
+export const getReadableDuration = (
+  _duration: number,
+  t: TFunction,
+  options: { shouldRound?: boolean; short?: boolean } = {}
+): string => {
+  const { shouldRound = false, short = false } = options;
   let duration = _duration;
+  let roundTo: 'hours' | 'minutes' | undefined;
 
   if (duration < 0 || Number.isNaN(duration)) {
     return '';
@@ -59,19 +57,25 @@ export const getReadableDuration = (_duration: number, t: TFunction) => {
   if (days > 0) {
     duration -= days * (MINUTES_IN_AN_HOUR * HOURS_IN_A_DAY);
     values['days'] = days;
+    if (shouldRound) {
+      roundTo = 'hours';
+    }
   }
 
   const hours = Math.floor(duration / MINUTES_IN_AN_HOUR);
   const minutes = duration % MINUTES_IN_AN_HOUR;
 
   if (hours > 0) {
-    values['hours'] = hours;
+    if (!roundTo && shouldRound) {
+      roundTo = 'minutes';
+    }
+    values['hours'] = roundTo === 'hours' ? Math.round(hours) : hours;
   }
-  if (minutes > 0) {
-    values['minutes'] = minutes;
+  if (minutes > 0 && (!roundTo || roundTo === 'minutes')) {
+    values['minutes'] = Math.round(minutes);
   }
 
-  return translateDuration(values, t);
+  return translateDuration(values, t, short);
 };
 
 const getPlural = (n: number, translationKey: 'hours' | 'minutes') => {
@@ -83,36 +87,42 @@ const getPlural = (n: number, translationKey: 'hours' | 'minutes') => {
 
 export const translateDuration = (
   values: { [key: string]: number },
-  t: TFunction
+  t: TFunction,
+  short?: boolean
 ) => {
   const { days, hours, minutes } = values;
   const resultArr = [];
 
-  if (days > 1) {
-    resultArr.push(t('common:days', { days }));
-  } else if (days === 1) {
-    resultArr.push(t('common:1day'));
+  if (short) {
+    if (days) resultArr.push(t('common:short.days', { days }));
+    if (hours) resultArr.push(t('common:short.hours', { hours }));
+    if (minutes) resultArr.push(t('common:short.minutes', { minutes }));
+  } else {
+    if (days > 1) {
+      resultArr.push(t('common:days', { days }));
+    } else if (days === 1) {
+      resultArr.push(t('common:1day'));
+    }
+
+    if (hours > 1) {
+      resultArr.push(t('common:' + getPlural(hours, 'hours'), { hours }));
+    } else if (hours === 1) {
+      resultArr.push(t('common:1hour'));
+    }
+
+    if (minutes > 1) {
+      resultArr.push(t('common:' + getPlural(minutes, 'minutes'), { minutes }));
+    } else if (minutes === 1) {
+      resultArr.push(t('common:1minute'));
+    }
   }
 
-  if (hours > 1) {
-    resultArr.push(t('common:' + getPlural(hours, 'hours'), { hours }));
-  } else if (hours === 1) {
-    resultArr.push(t('common:1hour'));
-  }
-
-  if (minutes > 1) {
-    resultArr.push(t('common:' + getPlural(minutes, 'minutes'), { minutes }));
-  } else if (minutes === 1) {
-    resultArr.push(t('common:1minute'));
-  }
-
+  const spacer = short ? ', ' : ' ' + t('common:and');
   if (resultArr.length === 3) {
-    return `${resultArr[0]}, ${resultArr[1]} ${t('common:and')} ${
-      resultArr[2]
-    }`;
+    return `${resultArr[0]}, ${resultArr[1]}${spacer} ${resultArr[2]}`;
   }
   if (resultArr.length === 2) {
-    return `${resultArr[0]} ${t('common:and')} ${resultArr[1]}`;
+    return `${resultArr[0]}${spacer} ${resultArr[1]}`;
   }
   return resultArr[0];
 };
