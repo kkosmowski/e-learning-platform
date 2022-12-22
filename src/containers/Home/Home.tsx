b56f398bbd9cc69c9efd67949ce41a6c51e6a8d7
kkosmowski <krzysztof.kosmowski@netguru.com>
2022-12-22 13:21:53 +0100

    Home adjustments

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,13 +1,17 @@
+import { Box } from '@mui/material';
+
 import HomeSidenav from 'containers/HomeSidenav';
-import { Centered } from 'shared/components/Container';
 import LayoutFix from 'layouts/LayoutFix';
+import CommonViewLayout from 'layouts/CommonView';
 
 export default function Home() {
   return (
     <LayoutFix>
       <HomeSidenav />
 
-      <Centered>Siemanko w HOME</Centered>
+      <CommonViewLayout>
+        <Box>Place for announcements...</Box>
+      </CommonViewLayout>
     </LayoutFix>
   );
 }
